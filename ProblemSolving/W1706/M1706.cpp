b9f08b8a7d0a9c34413edbe434c1ebcca00c3ce2
jaehwan.sim <jaehwan.sim@samsung.com>
2017-02-24 02:11:17 +0900

    commit latest code

---
+++ ProblemSolving/W1706/M1706.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int test_case;
+    freopen("sample_input_M1706.txt", "r", stdin);
+    setbuf(stdout, NULL);
+
+    scanf("%d", &test_case);
+
+    for(int T=1;T<=test_case;T++)
+    //for(int T=1;T<=1;T++)
+    {
+        int N;
+        double sum=0;
+
+        scanf("%d", &N);
+        for(int i=0;i<N;i++)
+        {
+            int x;
+            double p;
+
+            scanf("%lf %d", &p, &x);
+            sum += (double) x * p;
+        }
+        printf("#%d %.6lf\n", T, sum);
+    }
+
+    return 0;
+}
---
+++ ProblemSolving/W1706/MH1706.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int T;
+    freopen("sample_input_MH1706.txt", "r", stdin);
+    setbuf(stdout, NULL);
+
+    scanf("%d", &T);
+
+    for(int test_case=1;T<=test_case;T++)
+    {
+        
+    }
+
+    return 0;
+}
